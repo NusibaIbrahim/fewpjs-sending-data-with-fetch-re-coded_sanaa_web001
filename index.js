c93bf368ec817677048e18a338c5f7b24ac000ce
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-03 07:02:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+  function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    };
+     
+    // method: "POST" is missing from the object below
+let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+     
+    fetch("http://localhost:3000/dogs", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      })
+      .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+
